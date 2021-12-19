d442734cb17d6cffa2773c18821228df6be7170a
Andy Mroczkowski <a@mrox.co>
2021-12-18 22:32:46 -0500

    fix sorting

--- lib/utils.bash
+++ lib/utils.bash
@@ -42,7 +42,7 @@ fi
 
 sort_versions() {
   sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
-    LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
+    LC_ALL=C sort -t. -k 1,1n -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
 }
 
 list_github_tags() {
